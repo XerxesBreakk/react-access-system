1586f17ec9e7f6f0a7383fd825dfcf01e7e5aa79
XerxesBreakk <carlos.alexndr@gmail.com>
2024-01-30 22:50:32 -0500

    topbar - staff menu secure

--- src/scenes/global/Topbar.js
+++ src/scenes/global/Topbar.js
@@ -11,10 +11,10 @@ import Tooltip from "@mui/material/Tooltip";
 
 import { useContext, useEffect, useState } from "react";
 
-import { ColorModeContext, tokens } from "../../theme";
+import { ColorModeContext } from "../../theme";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import useAuth from "../../hooks/useAuth";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 /*
  */
@@ -23,11 +23,10 @@ const GET_USER_URL = "/auth/users/me/";
 
 const Topbar = () => {
   const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
 
   //toggle dark mode
   const colorMode = useContext(ColorModeContext);
-  
+
   const { state, user_loaded_success, login_fail } = useAuth();
 
   //Navigate config
@@ -55,32 +54,23 @@ const Topbar = () => {
   //User Management menu
   const [anchorUser, setAnchorUser] = useState(null);
   const openUser = Boolean(anchorUser);
-  const usersHandleClick = (event) => {
-    setAnchorUser(event.currentTarget);
-  };
-  const handleCloseUser = () => {
-    setAnchorUser(null);
-  };
-  
+
   //OT Management menu
   const [anchorOT, setAnchorOT] = useState(null);
   const openOT = Boolean(anchorOT);
-  const otHandleClick = (event) => {
-    setAnchorOT(event.currentTarget);
-  };
-  const handleCloseOT = () => {
-    setAnchorOT(null);
-  };
 
   //User Menu
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+
+  const handleMenuClick = (event, setAnchor) => {
+    setAnchor(event.currentTarget);
+  }
+
+  const handleCloseMenu = (setAnchor) => {
+    setAnchor(null);
+  }
+
   const handleCloseLogout = () => {
     setAnchorEl(null);
     login_fail();
@@ -89,7 +79,6 @@ const Topbar = () => {
   return (
     <Box
       display="flex"
-      sx={{ backgroundColor: colors.primary[400] }}
       justifyContent="space-between"
       p={2}
     >
@@ -101,59 +90,65 @@ const Topbar = () => {
         </IconButton>
       </Box>
       {/* Nav */}
-      <Box display="flex">
-        <Tooltip title="Administrador de usuarios">
-          <IconButton
-            aria-controls={openUser ? "users-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={openUser ? true : undefined}
-            onClick={usersHandleClick}
+      {state.is_authenticated ? (
+        <Box display="flex">
+          {state.user?.is_staff ? (
+            <>
+              <Tooltip title="Administrador de usuarios">
+                <IconButton
+                  aria-controls={openUser ? "users-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={openUser ? true : undefined}
+                  onClick={(event) => handleMenuClick(event, setAnchorUser)}
+                >
+                  <ManageAccountsIcon />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                id="users-menu"
+                anchorEl={anchorUser}
+                open={openUser}
+                onClose={() => handleCloseMenu(setAnchorUser)}
+                MenuListProps={{
+                  "aria-labelledby": "users-button",
+                }}
+              >
+                <MenuItem onClick={() => navigate("/users")}>Lista usuarios</MenuItem>
+                <MenuItem onClick={() => navigate("/users/create")}>
+                  Crear usuarios
+                </MenuItem>
+              </Menu>
+            </>
+          ) : null}
+          <Tooltip title="Ordenes de trabajo">
+            <IconButton
+              aria-controls={openOT ? "ot-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={openOT ? true : undefined}
+              onClick={(event) => handleMenuClick(event, setAnchorOT)}
+            >
+              <CalendarMonthIcon />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            id="ot-menu"
+            anchorEl={anchorOT}
+            open={openOT}
+            onClose={() => handleCloseMenu(setAnchorOT)}
+            MenuListProps={{
+              "aria-labelledby": "ot-button",
+            }}
           >
-            <ManageAccountsIcon />
-          </IconButton>
-        </Tooltip>
-        <Menu
-          id="users-menu"
-          anchorEl={anchorUser}
-          open={openUser}
-          onClose={handleCloseUser}
-          MenuListProps={{
-            "aria-labelledby": "users-button",
-          }}
-        >
-          <MenuItem onClick={() => navigate("/users")}>Lista usuarios</MenuItem>
-          <MenuItem onClick={() => navigate("/users/create")}>
-            Crear usuarios
-          </MenuItem>
-        </Menu>
-        <Tooltip title="Ordenes de trabajo">
-          <IconButton
-            aria-controls={openOT ? "ot-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={openOT ? true : undefined}
-            onClick={otHandleClick}
-          >
-            <CalendarMonthIcon />
-          </IconButton>
-        </Tooltip>
-        <Menu
-          id="ot-menu"
-          anchorEl={anchorOT}
-          open={openOT}
-          onClose={handleCloseOT}
-          MenuListProps={{
-            "aria-labelledby": "ot-button",
-          }}
-        >
-          <MenuItem onClick={() => navigate("/work-order")}>Ver orden de trabajo</MenuItem>
-          <MenuItem onClick={() => navigate("/work-order/create")}>
-            Crear orden de trabajo
-          </MenuItem>
-          <MenuItem onClick={() => navigate("/work-order/approve")}>
-            Aprobar OTs
-          </MenuItem>
-        </Menu>
-      </Box>
+            <MenuItem onClick={() => navigate("/work-order")}>Ver orden de trabajo</MenuItem>
+            <MenuItem onClick={() => navigate("/work-order/create")}>
+              Crear orden de trabajo
+            </MenuItem>
+            <MenuItem onClick={() => navigate("/work-order/approve")}>
+              Aprobar OTs
+            </MenuItem>
+          </Menu>
+        </Box>
+      ) : null}
       {/* ICONS */}
       <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
@@ -169,7 +164,7 @@ const Topbar = () => {
               aria-controls={open ? "user-menu" : undefined}
               aria-haspopup="true"
               aria-expanded={open ? true : undefined}
-              onClick={handleClick}
+              onClick={(event) => handleMenuClick(event, setAnchorEl)}
             >
               <PersonOutlinedIcon />
             </IconButton>
@@ -177,7 +172,7 @@ const Topbar = () => {
               id="user-menu"
               anchorEl={anchorEl}
               open={open}
-              onClose={handleClose}
+              onClose={() => handleCloseMenu(setAnchorEl)}
               MenuListProps={{
                 "aria-labelledby": "user-button",
               }}
